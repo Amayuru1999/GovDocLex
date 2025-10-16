c3afc9cdef319273edaa9678eab6e8c58ffde6b7
HirunaD <de_silva_pkhd_e22@engug.ruh.ac.lk>
2025-10-16 16:06:37 +0530

    add animations to features section

--- frontend/src/section/KeyFeatures.tsx
+++ frontend/src/section/KeyFeatures.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import KeyFeaturesCard from "../components/Cards/KeyFeaturesCard";
 import iconimg from "../assets/images/icon.png";
 import greenback from "../assets/images/greenback.png"; // Adjust the path as necessary
+import gsap from "gsap";
 
 const KeyFeaturesSection: React.FC = () => {
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    gsap.to(textRef.current, {
+      "--glow-pos": "100%",
+      repeat: -1,
+      duration: 3,
+      ease: "linear",
+      yoyo: true,
+    });
+  }, []);
+
   return (
     <section
       className="py-10  flex flex-col justify-center items-center bg-[#0b1016]  relative "
@@ -12,11 +25,23 @@ const KeyFeaturesSection: React.FC = () => {
       // }}
     >
       <div className="max-w-[1920px] mx-auto px-4  ">
-        <button className="bg-[#1AD3FF1A] text-[#1AD3FF] text-lg font-medium px-4 py-1.5 rounded-full">
+        <button className="bg-[#1AD3FF1A] hover:opacity-80 text-[#1AD3FF] text-lg hover:font-medium font-gilroy font-semibold px-4 py-1.5 rounded-full hover:scale-105 transition-all duration-300 ease-in-out mb-4"
+        data-aos="fade-down">
           Key Features
         </button>
-        <h2 className="text-4xl md:text-6xl font-semibold  text-white mb-6 ">
-          Beyond Extraction: Smart Analysis for Legal Documents
+        <h2
+          ref={textRef}
+          data-aos="fade-up"
+          className="text-transparent bg-clip-text bg-gradient-to-r from-[#FFFFFF] to-[#FFFFFF]/50 text-4xl md:text-6xl font-semibold mb-6 "
+          // className="text-transparent bg-clip-text bg-gradient-to-r from-[#FFFFFF] to-[#FFFFFF]/50 font-sfpro font-semibold text-4xl md:text-6xl px-4"
+          style={{
+            backgroundImage:
+              "linear-gradient(90deg, rgba(0,255,255,0.2) 0%, rgba(255,255,255,1) var(--glow-pos, 0%), rgba(0,255,255,0.2) 100%)",
+            backgroundSize: "200% 100%",
+          }}
+        >
+          Beyond Extraction: Smart Analysis for <br />{" "}
+          <span className="text-cyan-400">Legal Documents</span>
         </h2>
 
         <img
@@ -32,21 +57,25 @@ const KeyFeaturesSection: React.FC = () => {
               title="Automated Document Extraction"
               description="Precisely extracts structured data from unstructured government documents"
               icon={iconimg}
+              dataAos="fade-down-right"
             />
             <KeyFeaturesCard
               title="AI-Powered Legal Reasoning"
               description="Uses advanced AI reasoning to interpret extracted legal content, drawing logical connections and insights to support informed decision-making"
               icon={iconimg}
+              dataAos="fade-down-left"
             />
             <KeyFeaturesCard
               title="Sri Lanka-Optimized"
               description="Custom-trained on local government document structures"
               icon={iconimg}
+              dataAos="fade-up-right"
             />
             <KeyFeaturesCard
               title="Interactive Knowledge Graph"
               description="Visualizes relationships between ministries, laws, and policies"
               icon={iconimg}
+              dataAos="fade-up-left"
             />
           </div>
         </div>
--- frontend/src/section/home/About.tsx
+++ frontend/src/section/home/About.tsx
@@ -1,23 +1,41 @@
+import { useEffect, useRef } from "react";
 import greenball from "/assets/images/background/green_ball.png";
+import gsap from "gsap";
 
 function About() {
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    gsap.to(textRef.current, {
+      "--glow-pos": "100%",
+      repeat: -1,
+      duration: 3,
+      ease: "linear",
+      yoyo: true,
+    });
+  }, []);
+
   return (
-    <div className="py-4 xsm:p-6 sm:p-12 md:p-20 xl:p-24 relative max-w-[1920px] mx-auto mt-10 md:mt-0">
-      
-      
+    <div className="py-4 xsm:p-6 sm:p-12 md:p-20 relative max-w-[1920px] mx-auto mt-10 md:mt-0">
       Green ball background accent
       <img
         src={greenball}
         alt="greenball"
         className="hidden sm:block absolute z-0 bottom-20 xl:bottom-0 left-1/4 w-[400px] lg:w-[600px] xl:w-[800px] opacity-20"
       />
-
       {/* Dark themed container */}
       <div
-        className="relative z-10 p-6 sm:p-8 md:p-12 xl:p-16
+        ref={textRef}
+        className="relative z-10 p-6 sm:p-8 md:p-12 xl:p-24
                       bg-[#0E2324]/20 backdrop-blur-md border border-white/20 
                       rounded-3xl text-center flex flex-col items-center gap-4
-                      shadow-[0_4px_30px_rgba(0,0,0,0.1)] hover:shadow-[0_0_25px_rgba(9,204,244,0.4)] transition-all duration-300 ease-in-out"
+                      hover:shadow-[0_4px_30px_rgba(0,0,0,0.1)] shadow-[0_0_25px_rgba(9,204,244,0.4)] transition-all duration-300 ease-in-out
+                      "
+        style={{
+          backgroundImage:
+            "linear-gradient(90deg, rgba(0,255,255,0) 0%, rgba(0,255,255,0.3) var(--glow-pos, 0%), rgba(0,255,255,0) 100%)",
+          backgroundSize: "200% 100%",
+        }}
         data-aos="zoom-in"
       >
         {/* Label */}
@@ -25,7 +43,7 @@ function About() {
 
         {/* Heading */}
         <h1
-          className="text-transparent bg-clip-text bg-gradient-to-r 
+          className="text-transparent bg-clip-text bg-gradient-to-r font-semibold
                        from-white to-white/70 font-sfpro text-4xl md:text-6xl"
         >
           Built for Innovators, Loved by Everyone
@@ -39,9 +57,6 @@ function About() {
           structured, interconnected knowledge.
         </p>
       </div>
-
-
-
     </div>
   );
 }
